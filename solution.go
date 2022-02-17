10d6dbf8c80d453e4a81bc64a495e657346304cf
alexander_susha <alexander.susha@epicgames.com>
2022-02-17 18:08:39 +0300

    add my code and go.mod

--- solution.go
+++ solution.go
@@ -1,5 +1,9 @@
 package square
 
+import (
+	"math"
+)
+
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
@@ -8,5 +12,39 @@ package square
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+type sidesNumber int
+
+const SidesCircle sidesNumber = 0
+const SidesTriangle sidesNumber = 3
+const SidesSquare sidesNumber = 4
+
+func CalcSquare(sideLen float64, sidesNum sidesNumber) float64 {
+	return sidesNum.CalcSquare(sideLen)
+}
+
+func (s sidesNumber) CalcSquare(sideLen float64) float64 {
+	switch s {
+	case SidesCircle:
+		return circleArea(sideLen)
+	case SidesTriangle:
+		return triangleArea(sideLen)
+	case SidesSquare:
+		return squareArea(sideLen)
+	default:
+		return 0
+	}
+
+}
+
+func triangleArea(sideLen float64) float64 {
+	return math.Pow(sideLen, 2) * math.Sqrt(3) / 4
+}
+
+func squareArea(sideLen float64) float64 {
+	return math.Pow(sideLen, 2)
+
+}
+
+func circleArea(sideLen float64) float64 {
+	return math.Pi * math.Pow(sideLen, 2)
 }
